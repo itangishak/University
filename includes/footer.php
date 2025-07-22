7db7a8a3ece69017dd43aa9c0f6e183d5fc1bfad
Itangishaka J.Esterique <48064130+itangishak@users.noreply.github.com>
2025-07-22 10:41:03 +0200

    fix: restore large screen styles and load bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -25,6 +25,8 @@
         </div>
     </div>
 </footer>
-<script src="./assets/js/main.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-QmF9l6xEm+GZFQj5vJZgUyU9KJ0sI1yPTI1Wq3a3Zkm+M0iSZz7jl5AK+Hb6Xk9k" crossorigin="anonymous"></script>
+    <script src="./assets/js/main.js"></script>
 </body>
 </html>
