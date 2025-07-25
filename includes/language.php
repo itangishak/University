13ece7e1bd4d28b7304649c97da554d3c2fe0910
Itangishaka J.Esterique <itangishakajohnesterique@gmail.com>
2025-07-25 16:42:18 +0200

    Refactor redirect handling for root paths in language switcher to improve URL management

--- includes/language.php
+++ includes/language.php
@@ -34,7 +34,13 @@ if (isset($_GET['lang']) && in_array($_GET['lang'], ['fr', 'en'])) {
     }
     
     // Ensure proper path handling for different environments
-    if (strpos($current_path, '/University/') === false) {
+    if ($_SERVER['HTTP_HOST'] === 'uab.edu.bi' && ($current_path === '/' || $current_path === '/index.php')) {
+        $redirect_url = '/index.php';
+        if (count($query) > 0) {
+            $redirect_url .= '?' . http_build_query($query);
+        }
+    }
+    elseif (strpos($current_path, '/University/') === false) {
         // Handle root paths
         if ($current_path === '/' || $current_path === 'index.php') {
             $redirect_url = 'index.php';
