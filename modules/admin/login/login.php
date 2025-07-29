cea44f294344ff76c6506a8bd4bdc037762b081d
Itangishaka J.Esterique <itangishakajohnesterique@gmail.com>
2025-07-29 16:01:03 +0200

    c

--- modules/admin/login/login.php
+++ modules/admin/login/login.php
@@ -1,4 +1,8 @@
 <?php
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../../../config/config.php';
 require_once __DIR__ . '/../../../config/oauth.php';
 require_once __DIR__ . '/../../../includes/Auth.php';
